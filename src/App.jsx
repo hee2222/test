3cf91677d16cc9665d23bfe4f6b2324cdc96d3fa
heeee <k129036@gmail.com>
2024-02-22 11:11:40 +0900

    fixed scroll damping

--- src/App.jsx
+++ src/App.jsx
@@ -36,7 +36,8 @@ function App() {
         <color attach="background" args={['#ececec']} />
         <ScrollControls
           pages={play && !end ? 100 : 0}
-          damping={1}
+          damping={0.2}
+          maxSpeed={1}
           style={{
             top: '10px',
             left: '0px',
